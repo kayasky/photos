38d6fd1f1679c189d20b1bca3afde2843f3ba8b3
Satbir Jhuti <satbirjhuti@gmail.com>
2018-09-19 18:32:52 -0400

    Implement photoService, tests

--- src/app/services/photo.service.spec.ts
+++ src/app/services/photo.service.spec.ts
@@ -1,12 +1,66 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { fakeAsync, flush } from '@angular/core/testing';
+import { of } from 'rxjs';
 
+import { APP_CONSTANTS } from './../app.constants';
 import { PhotoService } from './photo.service';
 
+
 describe('PhotoService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: PhotoService;
+  let httpClient: jasmine.SpyObj<HttpClient>;
+
+  beforeEach(() => {
+    httpClient = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new PhotoService(httpClient);
+  });
 
   it('should be created', () => {
-    const service: PhotoService = TestBed.get(PhotoService);
     expect(service).toBeTruthy();
   });
+
+  describe('#getUsers', () => {
+    it('should fetch the users list', fakeAsync(() => {
+      const mockResponse = [{ 'userId': Math.random() }];
+      httpClient.get.and.returnValue(of(mockResponse));
+
+      service.getUsers().subscribe((response) => {
+        expect(httpClient.get).toHaveBeenCalledTimes(1);
+        expect(httpClient.get).toHaveBeenCalledWith(APP_CONSTANTS.url.getUsers);
+        expect(response).toEqual(mockResponse);
+        flush();
+      });
+    }));
+  });
+
+  describe('#getAlbumsForUser', () => {
+    it('should fetch the albums list for a particular user', fakeAsync(() => {
+      const mockResponse = [{ 'albumId': Math.random() }];
+      const userId = Math.random();
+      httpClient.get.and.returnValue(of(mockResponse));
+
+      service.getAlbumsForUser(userId).subscribe((response) => {
+        expect(httpClient.get).toHaveBeenCalledTimes(1);
+        expect(httpClient.get).toHaveBeenCalledWith(`${APP_CONSTANTS.url.getAlbums}${userId}`);
+        expect(response).toEqual(mockResponse);
+        flush();
+      });
+    }));
+  });
+
+  describe('#getPhotosInAlbum', () => {
+    it('should fetch the photos within a particular album', fakeAsync(() => {
+      const mockResponse = [{ 'photoId': Math.random() }];
+      const albumId = Math.random();
+      httpClient.get.and.returnValue(of(mockResponse));
+
+      service.getPhotosInAlbum(albumId).subscribe((response) => {
+        expect(httpClient.get).toHaveBeenCalledTimes(1);
+        expect(httpClient.get).toHaveBeenCalledWith(`${APP_CONSTANTS.url.getPhotos}${albumId}`);
+        expect(response).toEqual(mockResponse);
+        flush();
+      });
+    }));
+  });
+
 });
--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+
+import { APP_CONSTANTS } from '../app.constants';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<any> {
+    return this.http.get(APP_CONSTANTS.url.getUsers);
+  }
+
+  getAlbumsForUser(userId: number): Observable<any> {
+    return this.http.get(`${APP_CONSTANTS.url.getAlbums}${userId}`);
+  }
+
+  getPhotosInAlbum(albumId: number): Observable<any> {
+    return this.http.get(`${APP_CONSTANTS.url.getPhotos}${albumId}`);
+  }
 }
