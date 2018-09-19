3cb1c912e2268c5f83bac93063a7ae2f4951c195
Satbir Jhuti <satbirjhuti@gmail.com>
2018-09-19 19:35:16 -0400

    Add albums page

---
+++ src/app/pages/albums/albums.component.spec.ts
@@ -0,0 +1,89 @@
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+
+import { PhotoService } from '../../services/photo.service';
+import { AlbumsComponent } from './albums.component';
+import { fakeAsync, flush } from '@angular/core/testing';
+
+
+describe('AlbumsComponent', () => {
+  let component: AlbumsComponent;
+  let route: any;
+  let photoService: jasmine.SpyObj<PhotoService>;
+
+  beforeEach(() => {
+    photoService = jasmine.createSpyObj('PhotoService', ['getAlbumsForUser']);
+
+    route = {
+      snapshot: {
+        paramMap: {
+          get: jasmine.createSpy('get')
+        }
+      }
+    };
+
+    photoService.getAlbumsForUser.and.returnValue(of({}));
+
+    component = new AlbumsComponent(route, photoService);
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('#ngOnInit', () => {
+    beforeEach(() => {
+      spyOn(component, 'getAlbums');
+    });
+
+    it('should call showAlbums if userId is present', () => {
+      route.snapshot.paramMap.get.and.returnValue('1');
+
+      component.ngOnInit();
+
+      expect(component.getAlbums).toHaveBeenCalledTimes(1);
+      expect((<any> component).userId).toEqual('1');
+    });
+
+    it('should not call showAlbums if userId is absent', () => {
+      route.snapshot.paramMap.get.and.returnValue(undefined);
+
+      component.ngOnInit();
+
+      expect(component.getAlbums).not.toHaveBeenCalled();
+      expect((<any> component).userId).toBeUndefined();
+    });
+  });
+
+  describe('#getAlbums', () => {
+    it('should subscribe to photoService.getAlbumsForUser', () => {
+      (<any> component).userId = '123';
+
+      component.getAlbums();
+
+      expect(photoService.getAlbumsForUser).toHaveBeenCalledTimes(1);
+      expect(photoService.getAlbumsForUser).toHaveBeenCalledWith('123');
+    });
+
+    it('should set the albums array', fakeAsync(() => {
+      const mockAlbums = [{ albumId: '1' }, { albumId: '2' }, { albumId: '3' }];
+      photoService.getAlbumsForUser.and.returnValue(of(mockAlbums));
+
+      component.getAlbums();
+      flush();
+
+      expect(component.albums).toEqual(mockAlbums);
+    }));
+  });
+
+  describe('#OnDestroy', () => {
+    it('should unsubscribe from the main subscription', () => {
+      spyOn(component.subscriptions, 'unsubscribe');
+
+      component.OnDestroy();
+
+      expect(component.subscriptions.unsubscribe).toHaveBeenCalledTimes(1);
+    });
+  });
+
+});
---
+++ src/app/pages/albums/albums.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { PhotoService } from '../../services/photo.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-albums',
+  templateUrl: './albums.component.html',
+  styleUrls: ['./albums.component.css'],
+  providers: [PhotoService]
+})
+export class AlbumsComponent implements OnInit {
+
+  albums: Array<any>;
+  subscriptions: Subscription = new Subscription();
+  private userId: string;
+
+  constructor(private route: ActivatedRoute,
+    private photoService: PhotoService) { }
+
+  ngOnInit() {
+    this.userId = this.route.snapshot.paramMap.get('userId');
+
+    if (this.userId) {
+      this.getAlbums();
+    }
+  }
+
+  getAlbums() {
+    this.subscriptions.add(this.photoService.getAlbumsForUser(this.userId)
+      .subscribe((albums) => {
+        this.albums = albums;
+      }));
+  }
+
+  OnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
+
+}
